b4862a6542166cf6fa7e19707e48a8968cfa0e49
marcellszabo <marcell.szabo@gmail.com>
2022-03-30 16:50:08 +0200

    konfig fájl kezelés #2

--- tsm.py
+++ tsm.py
@@ -4,7 +4,7 @@ import os
 import datetime
 import configparser
 import getpass
-
+import subprocess
 from termcolor import colored
 from prompt_toolkit.completion import NestedCompleter
 from prompt_toolkit import PromptSession
@@ -14,18 +14,17 @@ from prompt_toolkit.cursor_shapes import CursorShape, ModalCursorShapeConfig
 class Dsmadmc:
     "This is the DSMADMC wrapper class"
     START_DSMADMC = 'dsmadmc'  # Ezzel a paranccsal indítom a TSM klienst - csak Mac-en tesztelve!
-    TSM_PROMPT = 'Protect: USERTSM.USR>'  # ez azért fontos, mert itt áll meg a pexpect
+    # TSM_PROMPT = 'Protect: USERTSM.USR>'  # ez azért fontos, mert itt áll meg a pexpect
     MORE = 'more...   \(\<ENTER\> to continue, \'C\' to cancel\)'  # meg itt
-    MORE2 = 'The character \'#\' stands for any decimal integer.' # meg itt
+    MORE2 = 'The character \'#\' stands for any decimal integer.'  # meg itt
     analyzer = None
     start = datetime.datetime.utcnow()
 
-
     def sendline(text):
         if Dsmadmc.analyzer is None or not Dsmadmc.analyzer.isalive():
-            STARTCOMMAND = Dsmadmc.START_DSMADMC+" -id="+id+" -pa="+pa
+            STARTCOMMAND = Dsmadmc.START_DSMADMC + " -id=" + id + " -pa=" + pa
             Dsmadmc.analyzer = pexpect.spawn('%s' % STARTCOMMAND, encoding='utf-8')
-            Dsmadmc.analyzer.expect([Dsmadmc.TSM_PROMPT, Dsmadmc.MORE, Dsmadmc.MORE2])
+            Dsmadmc.analyzer.expect([prompt, Dsmadmc.MORE, Dsmadmc.MORE2])
             if not Dsmadmc.analyzer.isalive():
                 print("Exception has occured, NO MORE respawn")
                 print(highlight(Dsmadmc.analyzer.before))
@@ -33,7 +32,8 @@ class Dsmadmc:
         columns, rows = os.get_terminal_size(0)
         Dsmadmc.analyzer.setwinsize(rows, columns)  ## hátha át lett méretezve az ablak
         Dsmadmc.analyzer.sendline(commandPreprocessor(text))  ## beküldjük a parancsot
-        Dsmadmc.analyzer.expect([Dsmadmc.TSM_PROMPT, Dsmadmc.MORE, Dsmadmc.MORE2, pexpect.EOF])  ## várjuk, hogy promptot vagy "MORE"-t kapjunk
+        Dsmadmc.analyzer.expect(
+            [prompt, Dsmadmc.MORE, Dsmadmc.MORE2, pexpect.EOF])  ## várjuk, hogy promptot vagy "MORE"-t kapjunk
         print(highlight(Dsmadmc.analyzer.before))  ## Highlighter-es kiíratás
         if Dsmadmc.analyzer.match_index == 1:
             commandline("more...   (<ENTER> to continue, 'C' to cancel)")
@@ -45,7 +45,9 @@ class Dsmadmc:
 
     def bye(self):
         end_date = datetime.datetime.utcnow()
-        print(colored('Thanks!', 'green'), colored('spadmin has run', 'white'), str(datetime.timedelta(seconds=abs((end_date - dsm.start).seconds))), colored('total','white'))
+        print(colored('Thanks!', 'green'), colored('spadmin has run', 'white'),
+              str(datetime.timedelta(seconds=abs((end_date - dsm.start).seconds))), colored('total', 'white'))
+
 
 def highlight(text):
     """a kapott szöveget sorokra szedi, majd ezeket színezi a megadott reguláris kif. segítségével"""
@@ -88,14 +90,15 @@ def commandPreprocessor(text):
     return text
 
 
-def commandline(prompt):
+def commandline(prmpt):
     """Command line interface"""
-    if prompt is not None:
+    if not prmpt:
         text = session.prompt(
-        prompt, completer=completer, complete_while_typing=False, cursor=CursorShape.BLINKING_BLOCK)
+            prompt, completer=completer, complete_while_typing=False, cursor=CursorShape.BLINKING_BLOCK)
     else:
         text = session.prompt(
-        Dsmadmc.TSM_PROMPT, completer=completer, complete_while_typing=False, cursor=CursorShape.BLINKING_BLOCK)  # Prompt, history-val és completerrel.
+            prmpt, completer=completer, complete_while_typing=False,
+            cursor=CursorShape.BLINKING_BLOCK)  # Prompt, history-val és completerrel.
     dsm.sendline(text)
 
 
@@ -110,15 +113,34 @@ def setup():
         id = input("Enter your user id: ")
     if pa is None or not pa:
         pa = getpass.getpass("Enter your password: ")
+    try:
+        result = subprocess.check_output(
+            ['dsmadmc', '-id=%s' % id, '-pa=%s' % pa, '-dataonly=yes', 'select SERVER_NAME from STATUS'])
+    except:
+        print (
+            "An error occured during the authentication, please check the error message above.")
+        quit(1)
+    if result:
+        print ("PROMPT: ", result.decode("utf-8").strip())
+        global prompt
+        prompt = 'Protect: %s>' % result.decode("utf-8").strip()
+    else:
+        print (
+            "An error occured during the authentication, please check your ISP connection, spadmin.ini file and/or userid-password pair")
+        print (result.decode("utf-8"))
+        quit(1)
+
     config['DEFAULT']['id'] = id
     config['DEFAULT']['password'] = pa
+
     with open('ispadmin.ini', 'w') as configfile:
         config.write(configfile)
 
+
 def welcome():
-   print('Let\'s start!')
-   print(colored('Welcome at', 'green'), colored('spadmin!', 'red'))
-   setup()
+    print('Let\'s start!')
+    print(colored('Welcome at', 'green'), colored('spadmin!', 'red'))
+    setup()
 
 
 if __name__ == "__main__":
