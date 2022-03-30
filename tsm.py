0f970cf0a9648d302554915e28f6e6321473843e
marcellszabo <marcell.szabo@gmail.com>
2022-03-30 14:00:19 +0200

    konfig fájl kezelés

--- tsm.py
+++ tsm.py
@@ -1,49 +1,66 @@
 import re
 import pexpect
 import os
+import datetime
+import configparser
+import getpass
+
 from termcolor import colored
 from prompt_toolkit.completion import NestedCompleter
 from prompt_toolkit import PromptSession
 from prompt_toolkit.cursor_shapes import CursorShape, ModalCursorShapeConfig
 
-START_DSMADMC = 'dsmadmc -id=support -pa=userkft1q2'  # Ezzel indítom a TSM klienst
-TSM_PROMPT = 'Protect: USERTSM.USR>'  # ez azért fontos, mert itt áll meg a pexpect
-MORE = 'more...   \(\<ENTER\> to continue, \'C\' to cancel\)'
+
+class Dsmadmc:
+    "This is the DSMADMC wrapper class"
+    START_DSMADMC = 'dsmadmc'  # Ezzel a paranccsal indítom a TSM klienst - csak Mac-en tesztelve!
+    TSM_PROMPT = 'Protect: USERTSM.USR>'  # ez azért fontos, mert itt áll meg a pexpect
+    MORE = 'more...   \(\<ENTER\> to continue, \'C\' to cancel\)'  # meg itt
+    MORE2 = 'The character \'#\' stands for any decimal integer.' # meg itt
+    analyzer = None
+    start = datetime.datetime.utcnow()
 
 
-def token(string):
-    """A paraméterben kapott string-et szavakra szedi. a Highlight függvény használja"""
-    # return string.split()
-    start = 0
-    i = 0
-    token_list = []
-    for x in range(0, len(string)):
-        if " " == string[i:i + 1][0]:
-            token_list.append(string[start:i + 1])
-            start = i + 1
-        elif "\n" == string[i:i + 1][0]:
-            token_list.append(string[start:i + 1])
-            start = i + 1
-        i += 1
-    token_list.append(string[start:i + 1])
-    return token_list
+    def sendline(text):
+        if Dsmadmc.analyzer is None or not Dsmadmc.analyzer.isalive():
+            STARTCOMMAND = Dsmadmc.START_DSMADMC+" -id="+id+" -pa="+pa
+            Dsmadmc.analyzer = pexpect.spawn('%s' % STARTCOMMAND, encoding='utf-8')
+            Dsmadmc.analyzer.expect([Dsmadmc.TSM_PROMPT, Dsmadmc.MORE, Dsmadmc.MORE2])
+            if not Dsmadmc.analyzer.isalive():
+                print("Exception has occured, NO MORE respawn")
+                print(highlight(Dsmadmc.analyzer.before))
+                quit(1)
+        columns, rows = os.get_terminal_size(0)
+        Dsmadmc.analyzer.setwinsize(rows, columns)  ## hátha át lett méretezve az ablak
+        Dsmadmc.analyzer.sendline(commandPreprocessor(text))  ## beküldjük a parancsot
+        Dsmadmc.analyzer.expect([Dsmadmc.TSM_PROMPT, Dsmadmc.MORE, Dsmadmc.MORE2, pexpect.EOF])  ## várjuk, hogy promptot vagy "MORE"-t kapjunk
+        print(highlight(Dsmadmc.analyzer.before))  ## Highlighter-es kiíratás
+        if Dsmadmc.analyzer.match_index == 1:
+            commandline("more...   (<ENTER> to continue, 'C' to cancel)")
+        if Dsmadmc.analyzer.match_index == 2:
+            commandline("The character '#' stands for any decimal integer.")
+        elif Dsmadmc.analyzer.match_index == 3:
+            Dsmadmc.bye(None)
+            quit();
 
+    def bye(self):
+        end_date = datetime.datetime.utcnow()
+        print(colored('Thanks!', 'green'), colored('spadmin has run', 'white'), str(datetime.timedelta(seconds=abs((end_date - dsm.start).seconds))), colored('total','white'))
 
 def highlight(text):
-    """a kapott szöveget szavakra szedi a token függvény hívásával, majd ezeket színezi a megadott reguláris kif. segítségével"""
+    """a kapott szöveget sorokra szedi, majd ezeket színezi a megadott reguláris kif. segítségével"""
     string = ""
-    word: text
-    for word in token(text):
+    for word in text.split('\n'):
         if re.search('Off', word):
-            string += (colored(word, 'red'))
+            string += (colored(word, 'red')) + "\n"
         elif re.search('(ANR\d\d\d\dE)', word):
-            string += (colored(word, 'red'))
+            string += (colored(word, 'red')) + "\n"
         elif re.search('(ANR\d\d\d\dW)', word):
-            string += (colored(word, 'yellow'))
+            string += (colored(word, 'yellow')) + "\n"
         elif re.search('([A-Za-z]{1,5}[0-9]{1,5}L[1-9])', word):  # LTO cartridges
-            string += (colored(word, 'green'))
+            string += (colored(word, 'green')) + "\n"
         else:
-            string += (word)
+            string += (word) + "\n"
     return string
 
 
@@ -65,33 +82,49 @@ completer = NestedCompleter.from_nested_dict({
 })
 
 
-def commandline():
+def commandPreprocessor(text):
+    if text == 'sh actlog':
+        return 'select DATE_TIME,MESSAGE from actlog WHERE (DATE_TIME>=current_timestamp-24 hour) order by 2 '
+    return text
+
+
+def commandline(prompt):
     """Command line interface"""
+    if prompt is not None:
+        text = session.prompt(
+        prompt, completer=completer, complete_while_typing=False, cursor=CursorShape.BLINKING_BLOCK)
+    else:
+        text = session.prompt(
+        Dsmadmc.TSM_PROMPT, completer=completer, complete_while_typing=False, cursor=CursorShape.BLINKING_BLOCK)  # Prompt, history-val és completerrel.
+    dsm.sendline(text)
 
-    text = session.prompt(
-        TSM_PROMPT, completer=completer, complete_while_typing=False, cursor=CursorShape.BLINKING_BLOCK
-    ) ## Prompt, history-val és completerrel.
 
-    if not analyzer.isalive():
-        print("Exception has occured, respawn required")
-        quit(1) ## ide kellene megírni az automatikus respawn funkciót
+def setup():
+    config = configparser.ConfigParser()
+    config.read('ispadmin.ini')
+    global id
+    global pa
+    id = config['DEFAULT']['id']
+    pa = config['DEFAULT']['password']
+    if id is None or not id:
+        id = input("Enter your user id: ")
+    if pa is None or not pa:
+        pa = getpass.getpass("Enter your password: ")
+    config['DEFAULT']['id'] = id
+    config['DEFAULT']['password'] = pa
+    with open('ispadmin.ini', 'w') as configfile:
+        config.write(configfile)
 
-    columns, rows = os.get_terminal_size(0)
-    analyzer.setwinsize(rows, columns) ## hátha át lett méretezve az ablak
-    analyzer.sendline(text) ## beküldjük a parancsot
-    analyzer.expect([TSM_PROMPT, MORE]) ## várjuk, hogy promptot vagy "MORE"-t kapjunk
-    print(highlight(analyzer.before)) ## Highlighter-es kiíratás
-    if analyzer.match_index == 1:
-        print("more...   (<ENTER> to continue, 'C' to cancel")
+def welcome():
+   print('Let\'s start!')
+   print(colored('Welcome at', 'green'), colored('spadmin!', 'red'))
+   setup()
 
 
 if __name__ == "__main__":
     """Main metódus"""
-    print('Let\'s start!')
-    print(colored('Hello', 'red'), colored('world!', 'green'))
-    analyzer = pexpect.spawn('%s' % START_DSMADMC, encoding='utf-8')
-    analyzer.expect([TSM_PROMPT, MORE])
+    welcome()
     session = PromptSession()
-
+    dsm = Dsmadmc
     while True:
-        commandline()
+        commandline(None)
