a7808ac817174d5359b088cd0f823beded46c0c6
_flex <gyorgy@fleischmann.hu>
2022-08-08 13:56:24 +0200

    Update spcompl.py

--- completer-poc/spcompl.py
+++ completer-poc/spcompl.py
@@ -15,7 +15,16 @@ from termcolor import colored
 if platform.system() == 'Windows':
     os.system('color')
 
-from pprint import pprint
+from pprint import pprint, pformat
+
+from re import search
+import logging
+
+logging.basicConfig( filename='spcompl.log',
+                     filemode='a',
+                     format=  '%(asctime)s %(levelname)s %(message)s',
+                     datefmt= '%Y%m%d %H%M%S',
+                     level=   logging.INFO )
 
 class SFACompleter:
     def __init__( self, rulefilename ):
@@ -40,31 +49,42 @@ class SFACompleter:
         
         consoleline( '#' )
         print( colored( 'Imported rules', 'green', attrs=[ 'bold' ] ) + ' from this file: [' + colored(  rulefilename, 'green' ) + ']' )
+        logging.info( 'Rule file imported:\n' + pformat( self.rules ) )
         pprint( self.rules )
         consoleline( '#' )
 
     def process( self, tokens ):
-        if len( tokens ) == 0:
-            return []
+        if len( tokens )   == 0:
+            # LEVEL ?
+            print( 'LEVEL ?' )
+            logging.info( 'Stepped into LEVEL 0.' )
+            ret = []
         elif len( tokens ) == 1:
+            # LEVEL 1
+            logging.info( 'Stepped into LEVEL 1.' )
             ret = [ x + ' ' for x in self.start if x.startswith( tokens[ -1 ] ) ]
-        else:
+        elif len( tokens ) == 2:
+            # LEVEL 2
+            logging.info( 'Stepped into LEVEL 2.' )
             ret = [ x + ' ' for x in self.rules[ tokens[ -2 ] ] if x.startswith( tokens[ -1 ] ) ]
+        elif len( tokens ) == 3:
+            # LEVEL 3
+            logging.info( 'Stepped into LEVEL >3.' )
+            ret = [ x + ' ' for x in self.rules[ tokens[ -2 ] ] if x.startswith( tokens[ -1 ] ) ]
+        else:
+            logging.info( 'Stepped into LEVEL Bzzz...' )
         return ret
 
     def complete( self, text, state ):
-        #print( '\n' )
-        #consoleline( 'D' )
-        #print( 'Text:  [' + text  + ']\n' )
-        #print( 'State: [' + state + ']\n' )
-        #consoleline( '-' )
-        #print( '\n' )
+        logging.info( 'COMPLETE Text: [' + text + '].' )
         try:
+            logging.info( 'readline buffer: [' + readline.get_line_buffer() + '].' )
             tokens = readline.get_line_buffer().split()
             if not tokens or readline.get_line_buffer()[ -1 ] == ' ':
                 tokens.append( '' )
-            results = self.process( tokens )+[ None ]
-            #consoleline( 'd' )
+            results = self.process( tokens ) + [ None ]
+            #logging.info( 'results: [' + results + '].' )
+            logging.info( 'results return: [' + results[ state ] + '].' )
             return results[ state ]   
         except Exception as e:
             print( coloreed( '\nOS error: {0}'.format(e), 'red' ) )
@@ -81,7 +101,7 @@ def consoleline( char ):
 completer = SFACompleter( "sprules.txt" )
 readline.set_completer( completer.complete )
 
-line = input( 'SP> ' )
+line = input( colored( 'SP>', 'white', 'on_green', attrs=[ 'bold' ] ) + ' ' )
 
 consoleline( '-' )
 print ( "You said: [" + line.strip() + "]" )
