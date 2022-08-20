8bdbf01ef1ce040544b9c7692cd8f7c5f9fe9631
_flex <gyorgy@fleischmann.hu>
2022-08-20 17:48:42 +0000

    SAVE

--- completer-poc/spadmin.py
+++ completer-poc/spadmin.py
@@ -317,43 +317,92 @@ class IBMSPrlCompleter:
       # sys.stdout.flush()
 
 class DSM:
-  
-      START_DSMADMC = "dsmadmc"
-      id            = "support"
-      pa            = "asdpoi123"
-      STARTCOMMAND  = START_DSMADMC + " -id=" + id + " -pa=" + pa + " -dataonly=yes" + " -comma"
-      MORE1         = 'more...   \(\<ENTER\> to continue, \'C\' to cancel\)'  # meg itt
-      MORE2         = 'The character \'#\' stands for any decimal integer.'  # meg itt
-      MORE3         = 'Do you wish to proceed\? \(Yes \(Y\)/No \(N\)\)'  # meg itt
-      PROMPT1       = 'Protect: .*'
-      PROMPT2       = 'tsm: .*'
-      tsm = None
-      
-      def get_tsm( self ):
-          if self.tsm is None or not self.tsm.isalive:
-              self.tsm = pexpect.spawn( '%s' % self.STARTCOMMAND, encoding = 'utf-8', echo = False )
-              self.tsm.setwinsize( 65534, 65534 )
-              self.tsm.expect( [ self.PROMPT1, self.PROMPT2, self.MORE1, self.MORE2, self.MORE3, pexpect.EOF ] )
-          return self.tsm
-      
-      def send_command( self, command ):
-          tsm = DSM.get_tsm( DSM )
-          try:
-              tsm.sendline( command )
-          except:
-              print( 'An error occurred during a dsmadmc execution. Please try again...' )
-              quit( 1 )
-          tsm.expect( [ self.PROMPT1, self.PROMPT2, self.MORE1, self.MORE2, self.MORE3, pexpect.EOF ] )
-          return tsm.before
-      
-      def send_command_array( self, command ):
-          list = DSM.send_command( DSM, command ).splitlines()
-          if len( list ) > 0:
-              list.pop( 0 )  # delete the first line which is the command itself
-          while ( '' in list ):  ## every output contains empty lines, we remove it
-              list.remove( '' )
-          return list
+    
+    START_DSMADMC = 'dsmadmc'
+    id            = 'support'
+    pa            = 'asdpoi123'
+    STARTCOMMAND  = START_DSMADMC + ' -id=' + id + ' -pa=' + pa + ' -dataonly=yes' + ' -comma'
+    MORE1         = 'more...   \(\<ENTER\> to continue, \'C\' to cancel\)'  # meg itt
+    MORE2         = 'The character \'#\' stands for any decimal integer.'  # meg itt
+    MORE3         = 'Do you wish to proceed\? \(Yes \(Y\)/No \(N\)\)'  # meg itt
+    PROMPT1       = 'Protect: .*'
+    PROMPT2       = 'tsm: .*'
+    tsm           = None
+    
+    def get_tsm( self ):
+        if self.tsm is None or not self.tsm.isalive:
+            self.tsm = pexpect.spawn( '%s' % self.STARTCOMMAND, encoding = 'utf-8', echo = False )
+            self.tsm.setwinsize( 65534, 65534 )
+            self.tsm.expect( [ self.PROMPT1, self.PROMPT2, self.MORE1, self.MORE2, self.MORE3, pexpect.EOF ] )
+        return self.tsm
 
+    def send_command( self, command ):
+        tsm = DSM.get_tsm( DSM )
+        logging.info( ' DSMADMC pid: [' + str( tsm.pid ) + ']' )
+        try:
+            tsm.sendline( command )
+        except:
+            print( 'An error occurred during a dsmadmc execution. Please try again...' )
+            quit( 1 )
+        tsm.expect( [ self.PROMPT1, self.PROMPT2, self.MORE1, self.MORE2, self.MORE3, pexpect.EOF ] )
+        return tsm.before
+    
+    def send_command_array( self, command ):
+        list = DSM.send_command( DSM, command ).splitlines()[ 1: ]
+        if len( list ) > 0:
+            list.pop( 0 )  # delete the first line which is the command itself
+        while ( '' in list ):  ## every output contains empty lines, we remove it
+            list.remove( '' )
+        return list
+
+class DSM2:
+          
+    START_DSMADMC = 'dsmadmc'
+    id            = 'support'
+    pa            = 'asdpoi123'
+    STARTCOMMAND  = START_DSMADMC + ' -id=' + id + ' -pa=' + pa
+    MORE1         = 'more...   \(\<ENTER\> to continue, \'C\' to cancel\)'  # meg itt
+    MORE2         = 'The character \'#\' stands for any decimal integer.'  # meg itt
+    MORE3         = 'Do you wish to proceed\? \(Yes \(Y\)/No \(N\)\)'  # meg itt
+    PROMPT1       = 'Protect: .*'
+    PROMPT2       = 'tsm: .*'
+    tsm           = None
+    
+    def get_tsm2( self ):
+        if self.tsm is None or not self.tsm.isalive:
+            self.tsm = pexpect.spawn( '%s' % self.STARTCOMMAND, encoding = 'utf-8', echo = False )
+            self.tsm.setwinsize( 65534, 65534 )
+            self.tsm.expect( [ self.PROMPT1, self.PROMPT2, self.MORE1, self.MORE2, self.MORE3, pexpect.EOF ] )
+        return self.tsm
+    
+    def send_command2( self, command ):
+        tsm = DSM2.get_tsm2( DSM2 )
+        logging.info( ' DSMADMC pid2: [' + str( tsm.pid ) + ']' )
+        try:
+            tsm.sendline( command )
+        except:
+            print( 'An error occurred during a dsmadmc execution. Please try again...' )
+            quit( 1 )
+        tsm.expect( [ self.PROMPT1, self.PROMPT2, self.MORE1, self.MORE2, self.MORE3, pexpect.EOF ] )
+        
+        # Session established with server CLOUDTSM1: Linux/x86_64
+        # Server Version 8, Release 1, Level 7.000
+        # Server date/time: 08/20/2022 19:12:44  Last access: 08/20/2022 16:48:38
+        
+        # Let's dance
+        ret = []
+        for i in tsm.before.splitlines()[ 1: ]:
+            if search( '^Session established with server \w+:', i ):
+                continue
+            elif search( '^Server Version \d+, Release \d+, Level \d+.\d\d\d', i ):
+                continue
+            elif search( '^Server date\/time\:', i ):
+                continue
+                
+            ret.append( i )
+            
+        return ret
+             
 #############      
 # Functions # ####################################################################
 #############
@@ -580,8 +629,8 @@ while True:
         continue
     
     # Command executor
-    consoleline( '-' )
-    print ( ' You said: [' + line.strip() + ']' )
+    #consoleline( '-' )
+    #print ( ' You said: [' + line.strip() + ']' )
     
     # Own commands
     if search( '^' + regexpgenerator( 'REload' ), line, IGNORECASE ):
@@ -598,9 +647,11 @@ while True:
         # Quit the program
         break
         
-    print( DSM.send_command( DSM, line ) )
+    for textline in DSM2.send_command2( DSM2, line ):    
+        if textline != '':
+            print( textline )
     
-    consoleline( '-' )
+    #consoleline( '-' )
 
 logging.info( consolefilledline( 'INPUT LOOP END ', '-', '', 120 ) )
 
