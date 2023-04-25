ae53f3b64114fcf07c7f6912d5b73c3528fc411e
_flex <gyorgy@fleischmann.hu>
2023-04-25 10:10:58 +0000

    s

--- src/lib/columnar.py
+++ src/lib/columnar.py
@@ -228,7 +228,7 @@ class Columnar:
 
         # Header 1st decorator line --------
         # out.write( colored( header_decorator[:globals.columns], globals.color_white, attrs=[ globals.color_attrs_bold ] ) + "\n")
-        out.write( utilities.color( header_decorator[:globals.columns], globals.color_white) + "\n")
+        out.write( utilities.color( header_decorator[:globals.columns].rstrip(), globals.color_white) + "\n")
 
         # Header
         header_line = ''
@@ -242,13 +242,15 @@ class Columnar:
 
         # Header 2nd decorator line --------
         # out.write( colored( header_decorator[:globals.columns], globals.color_white, attrs=[ globals.color_attrs_bold ] ) + "\n")
-        out.write( utilities.color( header_decorator[:globals.columns], globals.color_white ) + "\n")
+        out.write( utilities.color( header_decorator[:globals.columns].rstrip(), globals.color_white ) + "\n")
 
         # Rows
         for row in data:  # sorok kiíratása
             
             line = ''
+                        
             for i, cell in enumerate(row):  # cellák kiíratása
+                
                 lenght_of_row = sum(self.column_length) + clen(self.column_length) - 1
                 if (i + 1) == len(row) and globals.columns < lenght_of_row:
                     restlength = globals.columns - (sum(self.column_length[:-1]) + len(self.column_length) - 1)
@@ -258,6 +260,8 @@ class Columnar:
                         line += self.get_justified_cell_text(i, cell) + ' '
                     else:
                         line += self.get_justified_cell_text(i, cell)
+                
+            line = line.rstrip()
                     
             if globals.columns - (sum(self.column_length[:-1]) + len(self.column_length) - 1) < 0:
                 out.write( colorleft( line, globals.columns ) + '\n' )
