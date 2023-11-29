c5d3ec4721738f804d9f78e71b693ad83388647a
_flex <gyorgy@fleischmann.hu>
2023-11-29 15:11:26 +0100

    FICK

--- src/slowness_measurement.py
+++ src/slowness_measurement.py
@@ -1,7 +1,41 @@
 import time
 from re import search, IGNORECASE, escape, findall
 
-from lib import utilities
+#from lib import utilities
+
+def regexpgenerator(regexp):
+    savelastchar = ''
+    if regexp[-1] == '=':
+        savelastchar = regexp[-1] + '(?!.*\w+\s)'
+        regexp = regexp[: -1]
+    # # save v2 with regexp pattern
+    # match = search( '(=.*)$', regexp )
+    # if match:
+    #   savelastchar = match[ 1 ]
+    #   regexp = regexp.replace( match[ 1 ], '' )  
+
+    result = ''
+    for part in regexp.split():
+
+        if part[0].isupper():
+
+            tmpregexp = part
+            tmpstring = part
+            # print( part + str( time.time() ) )
+            for x in part:
+                if tmpstring[-1].isupper():
+                    break
+                tmpstring = part[0:len(tmpstring) - 1]
+                tmpregexp += '|' + tmpstring
+
+            result += '(' + tmpregexp + ')'
+
+        else:
+            result += '(' + part + ')'
+
+        result += '\s+'
+
+    return result[:-3] + savelastchar
 
 # 20231129 135234 DEBUG 13 and searching for regexp pattern [Query Volume [\w\*\-\.\/\\\$]+ (\w+=.*){1,} ACCess=]
 # 20231129 135234 DEBUG 13 and searching for regexp pattern [^(Query|Quer|Que|Qu|Q)\s+(Volume|Volum|Volu|Vol|Vo|V)\s+([\w\*\-\.\/\\\$]+)\s+((\w+=.*){1,})\s+(ACCess|ACCes|ACCe|ACC)=(?!.*\w+\s)]
@@ -9,6 +43,7 @@ from lib import utilities
 # 20231129 135236 INFO   DEBUG IF END ---------------- 
 
 key    = "Query Volume [\w\*\-\.\/\\\$]+ (\w+=.*){1,} ACCess="
+#           Query                   Volume                         [\w\*\-\.\/\\\$]+     (\w+=.*){1,}     ACCess=
 genkey = "^(Query|Quer|Que|Qu|Q)\s+(Volume|Volum|Volu|Vol|Vo|V)\s+([\w\*\-\.\/\\\$]+)\s+((\w+=.*){1,})\s+(ACCess|ACCes|ACCe|ACC)=(?!.*\w+\s)"
 tokens = ['query',
  'vol',
@@ -25,12 +60,85 @@ tokens = ['query',
 
 extradelimiter = '='
 
-print( time.time() )
+print( "1" )
+t = time.time()
+print( genkey )
+print( ' '.join( tokens ) ) 
+
+print( type( genkey ) )
+
 search( genkey, ' '.join( tokens ) )
-print( time.time() )
+print( time.time() - t )
 
 print()
 
-print( time.time() )
-search( '^' + utilities.regexpgenerator( key ) + extradelimiter, ' '.join( tokens ) + extradelimiter, IGNORECASE )
-print( time.time() )
+print( "1g" )
+t = time.time()
+print( genkey )
+print( ' '.join( tokens ) ) 
+
+print( type( genkey ) )
+
+search( regexpgenerator( key ), ' '.join( tokens ) )
+print( time.time() - t )
+
+print()
+
+print( "1i" )
+t = time.time() 
+search( genkey, ' '.join( tokens ), IGNORECASE )
+print( time.time() - t )
+
+print()
+
+print( "2i" )
+t = time.time() 
+print( regexpgenerator( key ) )
+print(' '.join( tokens ))
+alma = '^' + regexpgenerator( key ) + extradelimiter
+search( genkey, ' '.join( tokens ) + extradelimiter, IGNORECASE )
+print( time.time() - t )
+
+print()
+
+print( "3lower" )
+t = time.time() 
+search( ( '^' + regexpgenerator( key ) + extradelimiter ).lower(), (' '.join( tokens ) + extradelimiter ).lower() )
+print( time.time() - t )
+
+from string import ascii_letters, ascii_lowercase
+
+alpha, lower = [ s.encode( 'ascii' ) for s in [ ascii_letters, ascii_lowercase] ]
+table = bytes.maketrans( alpha, lower * 2 )           # convert to lowercase
+# deletebytes = bytes(set(range(256)) - set(alpha)) # delete nonalpha
+
+print()
+
+print( "4translate" )
+t = time.time() 
+print("AAAAAAAA".translate(table))
+search( ( '^' + regexpgenerator( key ) + extradelimiter ).translate( table ), ( ' '.join( tokens ) + extradelimiter ).translate( table ) )
+print( time.time() - t )
+
+print()
+
+print( "4translate" )
+
+a = str( '^' + regexpgenerator( key ) ).translate( table )
+b = str( ' '.join( tokens ) ).translate( table )
+
+t = time.time()
+print( type(a) )
+print( a )
+print( b )
+search( a, b )
+print( time.time() - t )
+
+print()
+
+print( "1" )
+t = time.time()
+print( genkey )
+print( ' '.join( tokens ) ) 
+search( genkey, ' '.join( tokens ) )
+print( time.time() - t )
